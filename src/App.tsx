9cea92ce288e0022a31f0a0d53b1dd5131a36eb6
Giorgi Mosemgvdlishvili <g_mosemgvdlishvili@yahoo.com>
2022-07-18 13:12:55 +0400

    update page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
+import Header from "./components/header/head";
 
 function App() {
   return (
-      <div>
-          <h1>Welcome to React Router!</h1>
+      <>
+          <Header/>
           <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/" element={<Home/>}/>
+              {/*<Route path="/hotels" element={<Hotels />} />*/}
+              {/*<Route path="/hotels/:id" element={<Hotel/>}/>*/}
+              {/*<Route path="/login" element={<Login/>}/>*/}
           </Routes>
-      </div>
+          {/*<Footer/>*/}
+      </>
   );
 }
 
