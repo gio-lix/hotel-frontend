fb950117e623f4997b14bcb6bd13727d6c495e7e
Giorgi Mosemgvdlishvili <gmosemgvdlishvili28@gmail.com>
2022-07-31 23:06:11 +0400

    update responsive page and add image flip

--- src/pages/hotel/index.tsx
+++ src/pages/hotel/index.tsx
@@ -8,11 +8,33 @@ import clsx from "clsx";
 import {SearchContext} from "../../context/SearchConext";
 import {AuthContext} from "../../context/AuthContext";
 import Reserved from "../../components/reserve";
+import {AiOutlineLeft, AiOutlineClose} from "react-icons/ai"
+
+const images = [
+    {
+        id: 1,
+        image: "https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/311380531.jpg?k=c46a39458dac7e0d19305f6d7dc00600d97665653f681d220da2378f57b67d0e&o=&hp=1"
+    },
+    {
+        id: 2,
+        image: "https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/311380580.jpg?k=5166c66040aa77067178a5b560e41e7bce09ade8e90e5b4d55935693b0cd2cce&o=&hp=1"
+    },
+    {
+        id: 3,
+        image: "https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/311387531.jpg?k=70cc118cbf4f40a207734fdc552a664a9a3763489c04e7337da44bda49f56d7c&o=&hp=1"
+    },
+    {
+        id: 4,
+        image: "https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/311380534.jpg?k=34f9ea66ced312003430baa1cd367b158245ce70067c1815520d90b91607db3c&o=&hp=1"
+    },
+]
 
 const Hotel = () => {
     const {id} = useParams()
     const navigate = useNavigate()
     const [openModal, setOpenModal] = useState<boolean>(false)
+    const [activeImage, setActiveImage] = useState(false)
+    const [activeImageIndex, setActiveImageIndex] = useState(0)
     const { data }: State = useFetch(`/hotels/find/${id}`);
 
 
@@ -38,6 +60,25 @@ const Hotel = () => {
         }
     }
 
+    const onHandleImageClick = (id: number) => {
+        if (window.innerWidth < 1024) return
+
+        setActiveImage(true)
+        setActiveImageIndex(id)
+    }
+
+
+    const onHandleChangeIndex = (item: string) => {
+        if (item === "plus") {
+            if (activeImageIndex === images.length - 1) return
+            setActiveImageIndex(prev => prev + 1)
+        }
+        if (item === "minus") {
+            if (activeImageIndex === 0) return
+            setActiveImageIndex(prev => prev - 1)
+        }
+    }
+
 
     return (
         <section className={clsx('container', s.root)}>
@@ -53,31 +94,28 @@ const Hotel = () => {
                 <button>Reserve or Book Now!</button>
             </article>
             <div className={clsx('flex', s.box)}>
+                {activeImage ? (
+                    <div className={s.activeImage}>
+                        <img src={images[activeImageIndex].image} alt="images"/>
+                        <span className={s.cansel} onClick={() => setActiveImage(false)}>
+                            <AiOutlineClose />
+                        </span>
+                        <div className={s.imageButton}>
+                            <button onClick={() => onHandleChangeIndex("minus")}>
+                                <AiOutlineLeft />
+                            </button>
+                            <button onClick={() => onHandleChangeIndex("plus")}>
+                                <AiOutlineLeft className={s.rotate}/>
+                            </button>
+                        </div>
+                    </div>
+                ) : null}
                 <div className={s.grid} style={{flexGrow: 1}}>
-                    <div>
-                        <img
-                            src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
-                            alt="img"
-                        />
-                    </div>
-                    <div>
-                        <img
-                            src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
-                            alt="img"
-                        />
-                    </div>
-                    <div>
-                        <img
-                            src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
-                            alt="img"
-                        />
-                    </div>
-                    <div>
-                        <img
-                            src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
-                            alt="img"
-                        />
-                    </div>
+                    {images?.map((img, index) => (
+                        <div key={img.id} onClick={() => onHandleImageClick(index)}>
+                            <img  src={img.image} alt="image"/>
+                        </div>
+                    ))}
                 </div>
                 <aside className={s.dateLengthBox}>
                     <h3>
